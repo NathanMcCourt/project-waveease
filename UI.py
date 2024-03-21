9c50d52797d0f03cc9e38513905f13a58dcca790
Jason Hankins <tul53850@temple.edu>
2024-03-21 13:32:28 -0400

    UI code

---
+++ UI.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import cv2 as cv
+
+window = tk.Tk()
+window.title("WAVEASE")
+window.minsize(500, 300)
+
+greeting = tk.Label(text="WELCOME TO WAVEASE")
+greeting.pack()
+
+window.mainloop() #listen for events
+
+
+
