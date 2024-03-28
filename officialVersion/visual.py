265bd2e7b5fbbedfa1f433172ec2f2eea2156e4c
shibatachun <q1093677764@gmail.com>
2024-03-27 23:43:25 -0400

    fixing

--- officialVersion/visual.py
+++ officialVersion/visual.py
@@ -1,5 +1,7 @@
 import cv2
 import mediapipe as mp
+import drawUtiles as dw
+
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 BaseOptions = mp.tasks.BaseOptions
@@ -12,13 +14,12 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 # Create a gesture recognizer instance with the live stream mode:
 def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
     print('gesture recognition result: {}'.format(result))
-
+    annotated_image = dw.draw_landmarks_on_image(frame, result)
+    cv2.imshow('Camera Feed', annotated_image)
 
 model_path = 'gesture_recognizer.task'
 base_options = BaseOptions(model_asset_path=model_path)
 
-def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-    print('gesture recognition result: {}'.format(result))
 options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.LIVE_STREAM,
@@ -31,16 +32,12 @@ with GestureRecognizer.create_from_options(options) as recognizer:
         if not success:
             break
 
-        # 将帧转换为MediaPipe的Image对象
         mp_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-        # 执行异步手势识别
+
         recognizer.recognize_async(mp_frame, int(cap.get(cv2.CAP_PROP_POS_MSEC)))
 
-        # 可视化（这部分取决于你如何实现回调函数print_result）
-        # ...
 
-        # 显示图像
-        cv2.imshow('Camera Feed', frame)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
