a749bd1adcc8ef1b21c4bdd86eba447b51dfe985
tun71427 <123014326+tun71427@users.noreply.github.com>
2024-03-27 23:30:22 -0400

    Refactor name

--- UI.py
+++ UI.py
@@ -71,7 +71,7 @@ settings = {
 }
 
 def start_gesture_recognition():
-    camera.StartCapture()
+    camera.start_capture()
     messagebox.showinfo("message", "recognition closed!")
 
 
@@ -131,7 +131,7 @@ def exit_app():
 
 # Create the main windows
 root = tk.Tk()
-root.title("WavEase!")
+root.title("Waveease!")
 root.geometry("800x600")  # initial size
 
 # layout
