a6ce7e2d16f18c1dd28a29f1f974d9beb695e266
shibatachun <q1093677764@gmail.com>
2024-03-28 00:01:20 -0400

    fixed using official example model for recognition. But without draw hand landmarks

--- officialVersion/visual.py
+++ officialVersion/visual.py
@@ -1,5 +1,7 @@
 import cv2
 import mediapipe as mp
+import numpy as np
+
 import drawUtiles as dw
 
 mp_drawing = mp.solutions.drawing_utils
@@ -14,19 +16,19 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 # Create a gesture recognizer instance with the live stream mode:
 def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
     print('gesture recognition result: {}'.format(result))
-    annotated_image = dw.draw_landmarks_on_image(frame, result)
-    cv2.imshow('Camera Feed', annotated_image)
 
 model_path = 'gesture_recognizer.task'
 base_options = BaseOptions(model_asset_path=model_path)
-
 options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.LIVE_STREAM,
     result_callback=print_result)
 with GestureRecognizer.create_from_options(options) as recognizer:
     cap = cv2.VideoCapture(0)
-
+    previous_position = None
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.5)
+    mp_draw = mp.solutions.drawing_utils
     while cap.isOpened():
         success, frame = cap.read()
         if not success:
@@ -36,8 +38,7 @@ with GestureRecognizer.create_from_options(options) as recognizer:
 
         recognizer.recognize_async(mp_frame, int(cap.get(cv2.CAP_PROP_POS_MSEC)))
 
-
-
+        cv2.imshow('Camera Feed', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
