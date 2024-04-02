27f62826fca4efa3915b9a89bf8c7b078c24c185
Jason Hankins <tul53850@temple.edu>
2024-04-02 13:14:41 -0400

    Display volume text on screen instead of terminal

--- gesture/volume.py
+++ gesture/volume.py
@@ -4,8 +4,44 @@
 #For recovery
 import cv2
 import mediapipe as mp
+import numpy as np
 import time
 import pyautogui
+
+
+
+def draw_info_text(image, gesture, brect):
+
+    info_text = gesture
+
+    cv2.putText(image, info_text, (brect[0] + 5, brect[1] - 4),
+               cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv2.LINE_AA)
+
+
+    return image
+
+
+
+def calc_bounding_rect(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_array = np.empty((0, 2), int)
+
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+
+        landmark_point = [np.array((landmark_x, landmark_y))]
+
+        landmark_array = np.append(landmark_array, landmark_point, axis=0)
+
+    x, y, w, h = cv2.boundingRect(landmark_array)
+
+    return [x, y, x + w, y + h]
+
+
+
+
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -42,20 +78,24 @@ def main():
                 thumb_ip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_IP]
                 fingers.insert(0, thumb_tip.x < thumb_ip.x)
 
+                rect = calc_bounding_rect(img, hand_landmarks)
+
                 # Detect finger pointing up
                 if fingers[1] and all(not f for f in fingers[2:]):
                     pyautogui.press('volumeup')
-                    print("Volume Up")
+                    #print("Volume Up")
+                    img = draw_info_text(img, "Volume Up", rect)
                 # Detect finger pointing down
                 elif not fingers[1] and all(not f for f in fingers[2:]):
                     pyautogui.press('volumedown')
-                    print("Volume Down")
+                    #print("Volume Down")
+                    img = draw_info_text(img, "Volume Down", rect)
 
                 # Print the corresponding number based on the number of fingers stretched out
-                if all(not f for f in fingers):
-                    print("6 - Fist")
-                else:
-                    print(f"{fingers.count(True)} - Fingers extended")
+                #if all(not f for f in fingers):
+                    #print("6 - Fist")
+                #else:
+                    #print(f"{fingers.count(True)} - Fingers extended")
 
                 mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
