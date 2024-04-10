bd0bc956848d7779d172e896fc49548eb66104d1
NathanMcCourt <tuk91355@temple.edu>
2024-04-10 14:03:10 -0400

    Initial set up for landmarks

--- gesture/loop.py
+++ gesture/loop.py
@@ -1,2 +1,25 @@
 #pyautogui for this gesture controls the keyboard and automates the command "Ctrl + r" to loop a song in a music player
 #Creator: Nathan McCourt
+
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+import pyautogui
+
+def draw_info_text(image, gesture, brect):
+    info_text = gesture
+    cv2.putText(image, info_text, (brect[0] + 5, brect[1] - 4),
+               cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv2.LINE_AA)
+    return image
+
+def calc_bounding_rect(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+    landmark_array = np.empty((0, 2), int)
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+        landmark_point = [np.array((landmark_x, landmark_y))]
+        landmark_array = np.append(landmark_array, landmark_point, axis=0)
+    x, y, w, h = cv2.boundingRect(landmark_array)
+    return [x, y, x + w, y + h]
