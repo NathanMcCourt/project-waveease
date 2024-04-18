a547b549c84ae14a6d1d42e840bfbf42e69b5c35
Jason Hankins <tul53850@temple.edu>
2024-04-18 13:56:08 -0400

    More style change

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ from cleanup import cleanup
 from tkinter import messagebox, ttk
 from PIL import Image, ImageTk
 
+import time
+import os
+
 # window = tk.Tk()
 # window.title("WAVEASE")
 # window.minsize(500, 300)
@@ -176,7 +179,7 @@ def launch_app():
 # Create the main windows
 root = tk.Tk()
 root.title("WavEase!")
-root.geometry("500x300")  # initial size
+root.geometry("500x400")  # initial size
 root.minsize(500, 300)
 root.configure(background="#87CEEB")
 
@@ -184,8 +187,8 @@ root.configure(background="#87CEEB")
 root.grid_columnconfigure([1,2], weight=1, minsize=70)
 #root.grid_columnconfigure(1, weight=1, minsize=70)
 
-root.grid_rowconfigure(0, weight=0, minsize=10)
-root.grid_rowconfigure(1, weight=1, minsize=20)
+root.grid_rowconfigure(0, weight=1, minsize=50)
+root.grid_rowconfigure(1, weight=1, minsize=10)
 #root.grid_rowconfigure([2,3], weight=1, minsize=10) ## does not work for Mac
 root.attributes('-alpha', 1.0) 
 
@@ -199,12 +202,25 @@ root.attributes('-alpha', 1.0)
 #   \ \/  \/ /    / _ \ | |  / __|  / _ \  | '_ ` _ \   / _ \   | __|  / _ \      \ \/  \/ /    / _` | \ \ / / |  __|    / _` | / __|  / _ \  
 #    \  /\  /    |  __/ | | | (__  | (_) | | | | | | | |  __/   | |_  | (_) |      \  /\  /    | (_| |  \ V /  | |____  | (_| | \__ \ |  __/  
 #     \/  \/      \___| |_|  \___|  \___/  |_| |_| |_|  \___|    \__|  \___/        \/  \/      \__,_|   \_/   |______|  \__,_| |___/  \___|""", background='#c3c3c3')
-label = tk.Label(root, text= r""" ༄ Welcome to WavEase ༄ """, background='#87CEEB')
+label = tk.Label(root, text= r""" ༄ Welcome to WavEase ༄ """, background='#87CEEB', fg="white")
 label.configure(font = ("Comic Sans MS", 28, "bold"))
-label.grid(row=1, column=1, sticky="nsew", padx=8, pady=8, columnspan=2)
+label.grid(row=0, column=1, sticky="nsew", padx=20, pady=10, columnspan=2)
 
 #tree graphic
+frameCnt = 120
+frames = [tk.PhotoImage(file='.assets/tree-01.gif',format = 'gif -index %i' %(i)) for i in range(frameCnt)]
 
+def update(ind):
+
+    frame = frames[ind]
+    ind += 1
+    if ind == frameCnt:
+        ind = 0
+    tree.configure(image=frame)
+    root.after(100, update, ind)
+tree = tk.Label(root, background='#87CEEB')
+tree.grid(row=1, column=1, sticky="nsew", columnspan=2)
+root.after(0, update, 0)
 
 # Add Button
 start_button = tk.Button(root, text="Start Recognition", command=start_gesture_recognition)
